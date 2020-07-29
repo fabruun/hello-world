1a63a8ace6a243e1ba43a18d7440183f9b722ee4
Frederik Bruun <frederikabruun@gmail.com>
2020-07-29 21:42:49 +0200

    updated gastsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,4 +7,8 @@
 module.exports = {
   /* Your site config here */
   plugins: [],
+  siteMetadata: {
+    title: `Test Page`,
+    siteUrl: process.env.CURRENT_DOMAIN || `https://admin.lashliftstylist-trineholmsted.dk`
+  }
 }
